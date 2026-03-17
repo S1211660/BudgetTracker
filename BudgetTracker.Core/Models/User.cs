649972a263915cf3cc2c085f47d044fc8820ff22
S1211660 <s1211660@student.windesheim.nl>
2026-03-17 12:11:46 +0100

    feat: add model for user
    Added a model for creating users.

---
+++ BudgetTracker.Core/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System.Transactions;
+
+namespace BudgetTracker.Core.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string PasswordHash { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public ICollection<Transaction> Transactions { get; set; } = new List<Transaction>();
+        public ICollection<Category> Categories { get; set; } = new List<Category>();
+    }
+}
